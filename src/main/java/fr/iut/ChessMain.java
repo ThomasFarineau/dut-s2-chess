adecaeb78e71c9559c33099faa50332478feab7d
PierreBH <pierre.bihannic@gmail.com>
2020-04-18 11:42:13 +0200

    test salut

--- src/main/java/fr/iut/ChessMain.java
+++ src/main/java/fr/iut/ChessMain.java
@@ -3,11 +3,12 @@ package fr.iut;
 public class ChessMain {
 
     public String getHelloMessage() {
-        return "Welcome to the chest game.";
+        return "Welcome to chest game.";
     }
 
     public static void main(String[] args) {
         final ChessMain chessMain = new ChessMain();
         System.out.println(chessMain.getHelloMessage());
+        System.out.println("salut");
     }
 }
