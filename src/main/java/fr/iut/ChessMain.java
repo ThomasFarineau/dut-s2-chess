e48036a2ea328bfc7a79791a148def4cd57023fd
Alex <alex.vu@etu.unice.fr>
2020-04-20 20:05:10 +0200

    découpage du main en différentes méthodes, placement du Scanner principal en attribut static du main

--- src/main/java/fr/iut/ChessMain.java
+++ src/main/java/fr/iut/ChessMain.java
@@ -6,6 +6,7 @@ import fr.iut.gestionpartie.GestionnairePartie;
 import fr.iut.plateau.Plateau;
 
 public class ChessMain {
+	private static Scanner sc = new Scanner(System.in); // On crée un scanner pour récupérer les entrées de l'utilisateur
 	private static Plateau p = new Plateau(); // On charge le plateau
 	private static GestionnairePartie gp = new GestionnairePartie(p); // On initialise un fichier avec le plateau
 
@@ -29,12 +30,40 @@ public class ChessMain {
 		char[] tabEntree = entree.toLowerCase().toCharArray();
 		return new int[] {tabEntree[0]-'a', 8-(tabEntree[1]-'0'), tabEntree[3]-'a', 8-(tabEntree[4]-'0')};
 	}
+	
+	public static void afficherMessageDebutTour() {
+		System.out.println("\n\n\n\n\n");
+		System.out.print("\n" +p+ "\n\nC'est au tour du joueur " + (p.getTourJoueur() ? "noir." : "blanc."));
+		
+		//Afficher si le roi du joueur est en echec
+		
+		System.out.println(p);
+	}
+	
+	public static void jouerTour() {
+		boolean entreeValide = false;
+		
+		while(!entreeValide) {
+			afficherMessageDebutTour();
+			
+			System.out.print("\nVeuillez entrer votre déplacement : ");
+			
+			String deplacement = sc.nextLine();
+			if(verifSyntaxe(deplacement)) {
+				try {
+					p.deplacer(convertChaine(deplacement));
+					entreeValide = true;
+				} catch(Exception e) {
+					System.out.println("Erreur : "+e.getMessage());
+				}
+			} else {
+				System.out.println("Erreur : Syntaxe invalide.");
+			}
+		}
+	}
 
 	public static void initialisation() {
-
 		System.out.println("Bienvenue dans le jeu d'échec.");
-
-		Scanner sc = new Scanner(System.in); // On implémente un scanner pour demander ce que veut l'utilisateur
 
 		boolean entree1Valide = false; // Sert à vérifier la validité du message saisi
 
@@ -60,14 +89,14 @@ public class ChessMain {
 
 			case "2":
 				boolean entree2Valide = false;
-
+				
 				while(!entree2Valide) {
 					System.out.print("\nVeuillez saisir le nom du fichier de partie (ex : \"maPartie.csv\"), entrez \"cancel\" pour revenir en arrière : ");
 					String entree2 = sc.nextLine(); // Un scan pour demander le nom du fichier
 					entree2Valide = true; // 
-					if(entree2.equals("cancel"))
+					if(entree2.equals("cancel")) {
 						entree1Valide = false;
-					else {
+					} else {
 						try {
 							gp.chargerAnciennePartie(entree2); // On charger le nom du fichier entrée
 						} catch(Exception e) {
@@ -84,43 +113,15 @@ public class ChessMain {
 				break;
 			}
 		}
-
-		sc.close();
 	}
 
 	public static void main(String[] args) {
 		initialisation();
 
-		Scanner sc = new Scanner(System.in); // On implémente un scanner pour demander ce que veut l'utilisateur
-
-		System.out.println("\n\n\n\n\n" + p);
-		System.out.println("\nLa partie commence !"); // On affiche le plateau pour commencer la partie.
+		System.out.println("\nLa partie commence !");
 
 		while(!p.verifMat()) {
-			System.out.print("\n\n\n\n\nC'est au tour des ");
-
-			if(p.getTourJoueur())
-				System.out.println("blancs");
-			else
-				System.out.println("noirs");
-
-
-			String deplacement = "";
-			boolean entreeValide = false;
-			while(!entreeValide) {
-				System.out.println("Veuillez entrer votre déplacement :");
-				deplacement = sc.nextLine();
-				if(verifSyntaxe(deplacement)) {
-					try {
-						p.deplacer(convertChaine(deplacement));
-						entreeValide = true;
-					} catch(Exception e) {
-						System.out.println("Erreur "+e.getMessage());
-					}
-				} else {
-					System.out.println("Erreur : Syntaxe invalide");
-				}
-			}
+			jouerTour();
 		}
 
 		sc.close();
