54f65084c7afd25ae4cc9a1fac22d3aab40b0ad0
Alex <alex.vu@etu.unice.fr>
2020-04-30 12:55:42 +0200

    correction du CavalierTest

--- src/test/java/fr/iut/pieces/CavalierTest.java
+++ src/test/java/fr/iut/pieces/CavalierTest.java
@@ -2,7 +2,6 @@ package fr.iut.pieces;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
