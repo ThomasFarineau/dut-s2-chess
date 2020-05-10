2127ab179eb4f05f3e183fa4c608a3b0ce02140e
miclo <miclo@DESKTOP-D2TKP18>
2020-05-10 19:16:22 +0200

    Finalisation de toStringTest de la classe Plateau

--- src/test/java/fr/iut/plateau/PlateauTest.java
+++ src/test/java/fr/iut/plateau/PlateauTest.java
@@ -22,7 +22,7 @@ public class PlateauTest {
 	}
 	
 	@Test
-	public void toStringTest() {
+	public void toStringTest1() {
 		try {
 			gp.chargerAnciennePartie("tests/partieTest.csv");
 		} catch (Exception e) {
@@ -38,6 +38,158 @@ public class PlateauTest {
 				"2  -   -   -   -   -   -   -   -  \n" + 
 				"1  -   -   -   -   -   -   -   -  \n" + 
 				"   A   B   C   D   E   F   G   H";
-		assertEquals(valeurAttendue, plat.toString());	
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest2() {
+		try {
+			gp.chargerAnciennePartie("tests/test2.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8  -   -   -   -   -   -  Fn  RoN \n" +
+				"7  -   -   -   -   -   -  Pb   -  \n" +
+				"6  -   -   -   -   -   -   -  RoB \n" +
+				"5  -   -   -   -   -   -   -   -  \n" +
+				"4  -   -   -   -   -   -   -   -  \n" +
+				"3  -   -   -   -   -   -   -   -  \n" +
+				"2  -   -   -   -   -   -   -   -  \n" +
+				"1  -   -   -   -   -   -   -   -  \n" +
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest3() {
+		try {
+			gp.chargerAnciennePartie("tests/test3.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8  -   -   -   -   -   -  Tn  RoN \n" + 
+				"7  -   -   -   -   -  Cb  Pn  Pn  \n" + 
+				"6  -   -   -   -   -   -   -   -  \n" + 
+				"5  -   -   -   -   -   -   -   -  \n" + 
+				"4  -   -   -   -   -   -   -   -  \n" + 
+				"3  -   -   -   -   -   -   -   -  \n" + 
+				"2  -   -   -   -   -  Pb  Pb  Pb  \n" + 
+				"1  -   -   -   -   -   -  RoB  -  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest4() {
+		try {
+			gp.chargerAnciennePartie("tests/test4.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8 Tn  Cn  Fn  ReN RoN Fn  Cn  Tn  \n" + 
+				"7 Pn  Pn  Pn  Pn  Pn  Pn  Pn  Pn  \n" + 
+				"6  -   -   -   -   -   -   -   -  \n" + 
+				"5  -   -   -   -   -   -   -   -  \n" + 
+				"4  -   -   -   -   -   -   -   -  \n" + 
+				"3  -   -   -   -   -   -   -   -  \n" + 
+				"2 Pb  Pb  Pb  Pb  Pb  Pb  Pb  Pb  \n" + 
+				"1 Tb  Cb  Fb  ReB RoB Fb  Cb  Tb  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest5() {
+		try {
+			gp.chargerAnciennePartie("tests/test5.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8  -   -   -   -   -   -   -   -  \n" + 
+				"7  -   -   -   -   -   -   -   -  \n" + 
+				"6  -   -   -   -   -   -   -   -  \n" + 
+				"5  -   -   -   -   -   -   -   -  \n" + 
+				"4  -   -   -  RoB  -   -   -   -  \n" + 
+				"3  -   -   -   -   -   -   -   -  \n" + 
+				"2  -   -  Cn   -   -   -   -   -  \n" + 
+				"1  -   -   -  RoN  -   -   -   -  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest6() {
+		try {
+			gp.chargerAnciennePartie("tests/test6.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8 Tn  Cn  Fn  ReN  -  Fn  Cn  Tn  \n" + 
+				"7 Pn  Pn  Pn  Pn  Pn  Pn  Pn  Pn  \n" + 
+				"6  -   -   -   -   -   -   -   -  \n" + 
+				"5  -   -   -   -   -   -   -   -  \n" + 
+				"4  -   -   -   -   -   -   -   -  \n" + 
+				"3 RoN  -   -   -   -   -   -   -  \n" + 
+				"2 Pb  Pb  Pb  Pb  Pb  Pb  Pb  Pb  \n" + 
+				"1 Tb  Cb  Fb  ReB RoB Fb  Cb  Tb  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest7() {
+		try {
+			gp.chargerAnciennePartie("tests/test7.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8  -   -   -  Tn   -   -  RoN  -  \n" + 
+				"7  -   -   -   -   -   -   -   -  \n" + 
+				"6  -   -   -   -   -   -   -   -  \n" + 
+				"5 Fn   -   -   -   -   -   -   -  \n" + 
+				"4  -   -   -   -   -   -   -   -  \n" + 
+				"3  -   -   -   -   -   -   -   -  \n" + 
+				"2  -   -   -   -  Pb  Pb   -   -  \n" + 
+				"1  -   -   -   -  RoB Fb   -   -  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest8() {
+		try {
+			gp.chargerAnciennePartie("tests/test8.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8 Tn   -  Fn  ReN RoN Fn   -  Tn  \n" + 
+				"7 Pn  Pn  Pn  Pn   -  ReB Pn  Pn  \n" + 
+				"6  -   -  Cn   -   -  Cn   -   -  \n" + 
+				"5  -   -   -   -  Pn   -   -   -  \n" + 
+				"4  -   -  Fb   -  Pb   -   -   -  \n" + 
+				"3  -   -   -   -   -   -   -   -  \n" + 
+				"2 Pb  Pb  Pb  Pb  Pb  Pb  Pb  Pb  \n" + 
+				"1 Tb  Cb  Fb   -  RoB  -  Cb  Tb  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
+	}
+	
+	@Test
+	public void toStringTest9() {
+		try {
+			gp.chargerAnciennePartie("tests/test9.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String valeurAttendue = "8  -   -   -  RoN  -   -   -   -  \n" + 
+				"7 Tb   -   -   -   -  Cb   -   -  \n" + 
+				"6  -   -   -   -   -  Fb   -   -  \n" + 
+				"5  -   -   -   -   -   -   -   -  \n" + 
+				"4  -   -   -   -   -   -   -   -  \n" + 
+				"3  -   -   -   -   -   -   -   -  \n" + 
+				"2  -   -   -   -   -   -   -   -  \n" + 
+				"1  -  RoB  -   -   -   -   -   -  \n" + 
+				"   A   B   C   D   E   F   G   H";
+		assertEquals(valeurAttendue, plat.toString());
 	}
 }
