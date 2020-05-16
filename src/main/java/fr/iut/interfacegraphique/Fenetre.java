6f402e8f4c46120a354b5c2284187aadafd8c352
Alex <alex.vu@etu.unice.fr>
2020-05-16 18:37:56 +0200

    avancement de l'interface graphique

--- src/main/java/fr/iut/interfacegraphique/Fenetre.java
+++ src/main/java/fr/iut/interfacegraphique/Fenetre.java
@@ -1,5 +1,6 @@
 package fr.iut.interfacegraphique;
 
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
@@ -12,65 +13,67 @@ import javax.swing.KeyStroke;
 import fr.iut.plateau.Plateau;
 
 public class Fenetre extends JFrame {
+	private ActionListener ml = null; // Il faut une classe qui implémente ActionListener pour le menu
+	
 	private JPanel jeu;
-	
-private JMenuBar menu = new JMenuBar();
-	
+
+	private JMenuBar menu = new JMenuBar();
+
 	private JMenu partie = new JMenu("Partie");
 	private JMenuItem nouvellePartie = new JMenuItem("Nouvelle Partie");
 	private JMenuItem chargerPartie = new JMenuItem("Charger une Partie...");
 	private JMenuItem enregistrerPartie = new JMenuItem("Enregistrer");
 	private JMenuItem enregistrerPartieSous = new JMenuItem("Enregistrer sous...");
-	
+
 	private JMenu informations = new JMenu("Informations");
 	private JMenuItem regles = new JMenuItem("Règles du jeu");
-	
-	public Fenetre(Plateau p) {
+
+	public Fenetre(Plateau p, ActionListener al) {
 		jeu = new PanneauJeu(p);
 		this.setContentPane(jeu);
-		this.initMenu();
-		
+		this.initMenu(ml);
+
 		this.pack();
-		
+
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("En Panne Corp. - Chess Game");
-		
+
 		this.setVisible(true);
 	}
-	
-	private void initMenu() {
+
+	private void initMenu(ActionListener al) {
 		nouvellePartie.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK));
-		nouvellePartie.addActionListener(null);
+		nouvellePartie.addActionListener(al);
 		partie.add(nouvellePartie);
-		
+
 		chargerPartie.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK));
-		chargerPartie.addActionListener(null);
+		chargerPartie.addActionListener(al);
 		partie.add(chargerPartie);
-		
+
 		enregistrerPartie.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK));
-		enregistrerPartie.addActionListener(null);
+		enregistrerPartie.addActionListener(al);
 		enregistrerPartie.setEnabled(false);
 		partie.add(enregistrerPartie);
-		
+
 		enregistrerPartieSous.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK + KeyEvent.SHIFT_DOWN_MASK));
-		enregistrerPartieSous.addActionListener(null);
+		enregistrerPartieSous.addActionListener(al);
 		enregistrerPartieSous.setEnabled(false);
 		partie.add(enregistrerPartieSous);
-		
+
 		regles.addActionListener(null);
 		informations.add(regles);
-		
+
 		menu.add(partie);
 		menu.add(informations);
-		
+
 		this.setJMenuBar(menu);
 	}
-	
+
 	public static void main(String[] args) {
 		Plateau p = new Plateau();
-		
-		JFrame f = new Fenetre(p);
+
+		JFrame f = new Fenetre(p, null);
 	}
 }
--- src/main/java/fr/iut/interfacegraphique/PanneauJeu.java
+++ src/main/java/fr/iut/interfacegraphique/PanneauJeu.java
@@ -1,38 +1,143 @@
 package fr.iut.interfacegraphique;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import fr.iut.gestionpartie.GestionnairePartie;
+import fr.iut.pieces.Piece;
 import fr.iut.plateau.Plateau;
 
 public class PanneauJeu extends JPanel {
 	private final static String imgPath = "./img/";
 	private Image fondEchiquier = null;
 	private Plateau plat;
+	private int[] selection = null; // coordonnées de la 1re sélection, si elle est valide
+	private boolean[][] selectionDeplacementsPoss; // déplacements possibles de la sélection
+	
+	private int[] casesEchec = null; // coordonnées des cases de l'échec (case du roi + case de la pièce qui le met en échec)
 	
 	public PanneauJeu(Plateau plat) {
 		this.plat = plat;
+		this.addMouseListener(null); // Remplacer null par le listener pour la souris quand il sera programmé
+		
+		//CODE TEMPORAIRE
+		GestionnairePartie gp = new GestionnairePartie(plat);
+		try {
+			gp.nouvellePartie();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		try {
 			fondEchiquier = ImageIO.read(new File(imgPath+"tempEchiquier.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		this.setCasesEchec(new int[] {0,1, 5,6});
+		//this.selectionner(6, 3);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
-		g.drawImage(fondEchiquier, this.getPreferredSize().width-fondEchiquier.getWidth(null), 0, null);
+		g.setColor(Color.red);
+		
+		int xOffset = this.getPreferredSize().width-560;
+		g.drawImage(fondEchiquier, 0, 0, null);
+		
+		Piece[][] echiquier = plat.getEchiquier();
+		
+		for (int i = 0; i < echiquier.length; i++) {
+			for (int j = 0; j < echiquier[i].length; j++) {
+				if (casesEchec != null) {
+					if ((casesEchec[0] == i && casesEchec[1] == j) || (casesEchec[2] == i && casesEchec[3] == j)) {
+						g.setColor(new Color(255, 150, 150));
+						g.fillRect(xOffset+70*j, 70*i, 70, 70);
+						
+						g.setColor(new Color(0, 0, 0, 75));
+						g.drawRect(xOffset+70*j, 70*i, 69, 69);
+						g.drawRect(xOffset+70*j+1, 70*i+1, 67, 67);
+					}
+				}
+				
+				if (selection != null) {
+					if (selection[0] == i && selection[1] == j) {
+						g.setColor(new Color(255, 255, 150));
+						g.fillRect(xOffset+70*j, 70*i, 70, 70);
+						
+						g.setColor(new Color(80, 0, 0, 75));
+						g.drawRect(xOffset+70*j, 70*i, 69, 69);
+						g.drawRect(xOffset+70*j+1, 70*i+1, 67, 67);
+						
+						
+						
+						for (int k = 0; k < selectionDeplacementsPoss.length; k++) {
+							for (int l = 0; l < selectionDeplacementsPoss[k].length; l++) {
+								if (selectionDeplacementsPoss[k][l]) {
+									g.setColor(new Color(150, 255, 255));
+									g.fillRect(xOffset+70*l, 70*k, 70, 70);
+									
+									g.setColor(new Color(0, 0, 0, 75));
+									g.drawRect(xOffset+70*l, 70*k, 69, 69);
+									g.drawRect(xOffset+70*l+1, 70*k+1, 67, 67);
+								}
+									
+							}
+						}
+					}
+				}
+				
+				if (echiquier[i][j] != null)
+					g.drawImage(echiquier[i][j].getImage(), xOffset+70*j, 70*i, null);
+			}
+		}
 	}
 	
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(600,600);
+	}
+	
+	public void selectionner(int i, int j) {
+		// La sélection ne fonctionne que si elle est sur une pièce de la bonne couleur
+		if (plat.getEchiquier()[i][j] != null) {
+			if (plat.getEchiquier()[i][j].getCouleur() == plat.getTourJoueur()) {
+				this.selection = new int[] {i,j};
+				this.selectionDeplacementsPoss = plat.calculerDeplacementsPiece(i, j);
+			}
+		} else {
+			this.resetSelection();
+		}
+	}
+	
+	public void resetSelection() {
+		this.selection = null;
+		this.selectionDeplacementsPoss = null;
+	}
+	
+	public boolean hasSelection() {
+		// Retourne si une case a été sélectionnée
+		return (selection != null);
+	}
+	
+	public void deplacer(int i, int j) {
+		try {
+			plat.deplacer(new int[] {selection[0], selection[1], i, j});
+		} catch (Exception e) {}
+		
+		resetSelection();
+	}
+	
+	public void setCasesEchec(int[] casesEchec) {
+		this.casesEchec = casesEchec;
 	}
 }
