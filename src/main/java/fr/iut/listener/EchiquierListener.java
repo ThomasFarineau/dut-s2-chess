8ee0bb16ed3ab2fbc4e335244519129bc3438ea0
Alex <alex.vu@etu.unice.fr>
2020-05-17 21:28:16 +0200

    Déplacement du listener de l'échiquier vers une Class à part entière

---
+++ src/main/java/fr/iut/listener/EchiquierListener.java
@@ -0,0 +1,39 @@
+package fr.iut.listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import fr.iut.interfacegraphique.PanneauJeu;
+
+public class EchiquierListener implements MouseListener {
+	private PanneauJeu listenedPanel = null;
+	
+	public EchiquierListener(PanneauJeu listenedPanel) {
+		this.listenedPanel = listenedPanel;
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		int x = (e.getX() >= 40 ?((e.getX()-40)/70) : -1);
+		int y = (e.getY()/70);
+		
+		listenedPanel.selectionner(y, x);
+		listenedPanel.repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+}
