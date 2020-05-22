6e865cf0feb73e3068021449781ba825e133b7f1
miclo <miclo@DESKTOP-D2TKP18>
2020-05-22 10:14:53 +0200

    Test des déplacements possibles pour les pions Blancs et Noirs

--- src/test/java/fr/iut/pieces/PionTest.java
+++ src/test/java/fr/iut/pieces/PionTest.java
@@ -1,4 +1,4 @@
-  
+
 package fr.iut.pieces;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -9,21 +9,71 @@ import org.junit.jupiter.api.Test;
 
 
 public class PionTest {
-	
-	private Pion pionNoir;
-    private Pion pionBlanc;
 
-    @BeforeEach
-    public void initialisation() {
-        pionNoir = new Pion(true);
-        pionBlanc = new Pion(false);
-    }
-	
+	private Pion pionNoir;
+	private Pion pionBlanc;
+
+	@BeforeEach
+	public void initialisation() {
+		pionNoir = new Pion(true);
+		pionBlanc = new Pion(false);
+	}
+
 	@Test
 	public void toStringTest() {
-	
+
 		assertEquals("Pb", pionBlanc.toString());
 		assertEquals("Pn", pionNoir.toString());
 	}
-	
-}
+
+	@Test
+	public void getDeplacementsPossPionBlancTest() {
+		boolean[][] deplacementsAttendus = { //                   centre
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, true, true, true, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}, // centre
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+				{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}
+		};
+
+		boolean[][] deplacementsCalculés = pionBlanc.getDeplacementsPoss();
+		assertArrayEquals(deplacementsAttendus, deplacementsCalculés);
+	}
+
+
+		@Test
+		public void getDeplacementsPossPionNoirTest() {
+			boolean [][] deplacementsAttendus = { //                   centre
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}, // centre
+					{false, false, false, false, false, false, true, true, true, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
+					{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}
+			};
+
+
+			boolean[][] deplacementsCalculés = pionNoir.getDeplacementsPoss();
+			assertArrayEquals(deplacementsAttendus, deplacementsCalculés);
+		}
+
+	}
