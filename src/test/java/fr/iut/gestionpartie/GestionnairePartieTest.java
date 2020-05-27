f30355f68e7cb088eb102ce16f505bb6974d962e
Emeric Maximil <maximil.emeric@gmail.com>
2020-05-27 16:14:43 +0200

    Avancement des tests GestionnairePartie (Emeric Jehan)

--- src/test/java/fr/iut/gestionpartie/GestionnairePartieTest.java
+++ src/test/java/fr/iut/gestionpartie/GestionnairePartieTest.java
@@ -2,6 +2,7 @@ package fr.iut.gestionpartie;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
@@ -30,7 +31,7 @@ public class GestionnairePartieTest {
 		gp = new GestionnairePartie(p);
 	}
 
-	
+
 	@Test
 	public void chargerAnciennePartieExceptionsTest() {
 		try {
@@ -40,7 +41,7 @@ public class GestionnairePartieTest {
 		} catch (Exception e) {
 			assertEquals("Fichier introuvable : \"fichierInexistant.csv\"", e.getMessage());
 		}
-		
+
 		try {
 			gp.chargerAnciennePartie("fichierInexistant2");
 			//Si aucune exception n'est générée, le test échoue
@@ -48,7 +49,7 @@ public class GestionnairePartieTest {
 		} catch (Exception e) {
 			assertEquals("Fichier introuvable : \"fichierInexistant2.csv\"", e.getMessage());
 		}
-		
+
 		try {
 			gp.chargerAnciennePartie("tests/tropDeColonnes2.csv");
 			//Si aucune exception n'est générée, le test échoue
@@ -56,7 +57,7 @@ public class GestionnairePartieTest {
 		} catch (Exception e) {
 			assertEquals("Ligne 7 invalide dans le fichier \"tests/tropDeColonnes2.csv\"", e.getMessage());
 		}
-		
+
 		try {
 			gp.chargerAnciennePartie("tests/pieceInconnue.csv");
 			//Si aucune exception n'est générée, le test échoue
@@ -64,7 +65,7 @@ public class GestionnairePartieTest {
 		} catch (Exception e) {
 			assertEquals("Nom de pièce invalide \"poutre\" à la ligne 5 du fichier \"tests/pieceInconnue.csv\"", e.getMessage());
 		}
-		
+
 		try {
 			gp.chargerAnciennePartie("tests/formatInvalide.csv");
 			//Si aucune exception n'est générée, le test échoue
@@ -73,7 +74,7 @@ public class GestionnairePartieTest {
 			assertEquals("Format du fichier \"tests/formatInvalide.csv\" invalide", e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void chargerAnciennePartieTest() {		
 		//exemple de chargement valide
@@ -106,21 +107,21 @@ public class GestionnairePartieTest {
 			fail();
 		}
 		Piece[][] echiquierACharger ={
-		{new Tour(true), new Cavalier(true), new Fou(true), new Reine(true), new Roi(true), new Fou(true), new Cavalier(true), new Tour(true)},
-	    {new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true)},
-	    {null, null, null, null, null, null, null, null},
-	    {null, null, null, null, null, null, null, null},
-	    {null, null, null, null, null, null, null, null},
-	    {null, null, null, null, null, null, null, null},
-	    {new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false)},
-	    {new Tour(false), new Cavalier(false), new Fou(false), new Reine(false), new Roi(false), new Fou(false), new Cavalier(false), new Tour(false)}
+				{new Tour(true), new Cavalier(true), new Fou(true), new Reine(true), new Roi(true), new Fou(true), new Cavalier(true), new Tour(true)},
+				{new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true)},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false)},
+				{new Tour(false), new Cavalier(false), new Fou(false), new Reine(false), new Roi(false), new Fou(false), new Cavalier(false), new Tour(false)}
 		};
-		
+
 		assertArrayEquals(echiquierACharger, p.getEchiquier());
 
 	}
-	
-	
+
+
 	@Test
 	public void sauvegarderPartieSansParamTest() {
 		Piece[][] echiquierASave = {
@@ -133,14 +134,6 @@ public class GestionnairePartieTest {
 				{null, null, null, null, null, null, null, null},
 				{null, null, null, null, null, null, null, null}
 		};
-		Plateau aSave = new Plateau();
-		aSave.setEchiquier(echiquierASave);
-		GestionnairePartie gp2= new GestionnairePartie(aSave);
-		try {
-			gp2.sauvegarderPartie("tests/sauvegarde1.csv");
-		} catch (Exception e) {
-			fail();
-		}
 
 		String[] echiquierAttendu= {
 				"RoB,V,ReN,V,V,V,V,V",
@@ -150,42 +143,57 @@ public class GestionnairePartieTest {
 				"V,V,V,V,V,V,V,V",
 				"V,V,V,V,V,V,V,V",
 				"V,V,V,V,V,V,V,V",
-				"V,V,V,V,V,V,V,V"};
-		Scanner scan;
-		try {
-			File sauvegarde1 = new File("parties/tests/sauvegarde1.csv");
-			scan = new Scanner(sauvegarde1);
-			int id=0;
-			while (scan.hasNext()) {
-				String ligneString = scan.nextLine();
-				assertEquals(ligneString, echiquierAttendu[id]);
-				id++;
-			}
-			scan.close();
-			sauvegarde1.delete();
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		"V,V,V,V,V,V,V,V"};
+
 	}
 
 	@Test
 	public void sauvegarderPartieAvecParamTest() {
 		Piece[][] echiquierASave ={
 				{new Tour(true), new Cavalier(true), new Fou(true), new Reine(true), new Roi(true), new Fou(true), new Cavalier(true), new Tour(true)},
-			    {new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true)},
-			    {null, null, null, null, null, null, null, null},
-			    {null, null, null, null, null, null, null, null},
-			    {null, null, null, null, null, null, null, null},
-			    {null, new Pion(false), null, null, null, null, null, null},
-			    {new Pion(false), null, new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false)},
-			    {new Tour(false), new Cavalier(false), new Fou(false), new Reine(false), new Roi(false), new Fou(false), new Cavalier(false), new Tour(false)}
-				};
-		Plateau aSave = new Plateau();
-		aSave.setEchiquier(echiquierASave);
-		GestionnairePartie gp2= new GestionnairePartie(aSave);
+				{new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true), new Pion(true)},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null},
+				{null, new Pion(false), null, null, null, null, null, null},
+				{new Pion(false), null, new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false), new Pion(false)},
+				{new Tour(false), new Cavalier(false), new Fou(false), new Reine(false), new Roi(false), new Fou(false), new Cavalier(false), new Tour(false)}
+		};
+
+		p.setEchiquier(echiquierASave);
+
 		try {
-			gp2.sauvegarderPartie("tests/sauvegarde1.csv");
+			gp.sauvegarderPartie("tests/nouvellePartie.csv");
+			//Si aucune exception n'est générée, le test échoue
+			fail();
+		} catch (Exception e) {
+			assertEquals("Vous n'avez pas le droit de sauvegarder une partie de nom \"nouvellePartie.csv\".", e.getMessage());
+		}
+
+		try {
+			gp.sauvegarderPartie("nouvellepartie.csv");
+			//Si aucune exception n'est générée, le test échoue
+			fail();
+		} catch (Exception e) {
+			assertEquals("Vous n'avez pas le droit de sauvegarder une partie de nom \"nouvellePartie.csv\".", e.getMessage());
+		}
+
+		try {
+			gp.sauvegarderPartie("../parties/nouvEllePartie");
+			//Si aucune exception n'est générée, le test échoue
+			fail();
+		} catch (Exception e) {
+			assertEquals("Vous n'avez pas le droit de sauvegarder une partie de nom \"nouvellePartie.csv\".", e.getMessage());
+		}
+		
+		File f = new File("parties/tests/sauvegarde1.csv");
+		if(f.exists())
+			f.delete();
+		
+		try {
+			String retourAttendu ="Le fichier \"tests/sauvegarde1.csv\" vient d'être créé. "+
+					"La sauvegarde vers \"tests/sauvegarde1.csv\" a été effectuée avec succès !";
+			assertEquals(retourAttendu,gp.sauvegarderPartie("tests/sauvegarde1.csv"));
 		} catch (Exception e) {
 			fail();
 		}
@@ -198,19 +206,57 @@ public class GestionnairePartieTest {
 				"V,V,V,V,V,V,V,V",
 				"V,Pb,V,V,V,V,V,V",
 				"Pb,V,Pb,Pb,Pb,Pb,Pb,Pb",
-				"Tb,Cb,Fb,ReB,RoB,Fb,Cb,Tb"};
+		"Tb,Cb,Fb,ReB,RoB,Fb,Cb,Tb"};
+
 		Scanner scan;
+
+
 		try {
-			scan = new Scanner(new File("parties/tests/sauvegarde1.csv")); //a changer
-			int id=0;
-			while (scan.hasNext()) {
-				String ligneString = scan.nextLine();
-				assertEquals(ligneString, echiquierAttendu[id]);
-				id++;
+			scan = new Scanner(f); 
+
+			for(String s : echiquierAttendu) {
+				assertEquals(s, scan.nextLine());
 			}
+
+			assertFalse(scan.hasNextLine());
 			scan.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			assertEquals("tests/sauvegarde1.csv", gp.getNomFichier());
+		} catch (Exception e) {
+			fail();
+		}
+
+		try {
+			p.setTourJoueur(true);
+			p.deplacer(new int[] {0, 1, 2, 0});
+			String retourAttendu = "La sauvegarde vers \"tests/sauvegarde1.csv\" a été effectuée avec succès !";
+			assertEquals(retourAttendu, gp.sauvegarderPartie("tests/sauvegarde1.csv"));
+		} catch (Exception e) {
+			fail();
+		}
+
+		String[] echiquierAttendu2= {
+				"Tn,V,Fn,ReN,RoN,Fn,Cn,Tn",
+				"Pn,Pn,Pn,Pn,Pn,Pn,Pn,Pn",
+				"Cn,V,V,V,V,V,V,V",
+				"V,V,V,V,V,V,V,V",
+				"V,V,V,V,V,V,V,V",
+				"V,Pb,V,V,V,V,V,V",
+				"Pb,V,Pb,Pb,Pb,Pb,Pb,Pb",
+		"Tb,Cb,Fb,ReB,RoB,Fb,Cb,Tb"};
+
+		try {
+			scan = new Scanner(f);
+
+			for(String s : echiquierAttendu2) {
+				assertEquals(s, scan.nextLine());
+			}
+
+			assertFalse(scan.hasNextLine());
+			scan.close();
+			assertEquals("tests/sauvegarde1.csv", gp.getNomFichier());
+			f.delete();
+		} catch (Exception e) {
+			fail();
 		}
 	}	
 }
