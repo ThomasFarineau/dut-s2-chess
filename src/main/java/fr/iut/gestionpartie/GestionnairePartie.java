7e678ab78961397aae6dc852b6ad9331e8b2cd3b
Alex <alex.vu@etu.unice.fr>
2020-05-29 18:28:59 +0200

    ajout d'une méthode setTourJoueur() pour GestionnairePartie

--- src/main/java/fr/iut/gestionpartie/GestionnairePartie.java
+++ src/main/java/fr/iut/gestionpartie/GestionnairePartie.java
@@ -182,8 +182,8 @@ public class GestionnairePartie {
 	public static Path getPartiesPath() {
 		return partiesPath;
 	}
-
-	public Plateau getPlat() {
-		return plat;
+	
+	public void setTourJoueur(boolean tour) {
+		plat.setTourJoueur(tour);
 	}
 }
