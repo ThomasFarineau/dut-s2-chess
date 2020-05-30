4670c5ba3d0b591c97f3a7d4bcbac83767c79542
flori <flori@DELL-Florian>
2020-05-30 16:19:48 +0200

    (suppression d'un import inutile)

--- src/test/java/fr/iut/pieces/CavalierTest.java
+++ src/test/java/fr/iut/pieces/CavalierTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import fr.iut.pieces.Cavalier;
-
 public class CavalierTest {
 	private Cavalier cavalierBlanc;
 	private Cavalier cavalierNoir;
